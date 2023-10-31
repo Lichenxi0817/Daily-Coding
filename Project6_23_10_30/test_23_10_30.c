a7f7162491339b43630ca145d82d997510943c51
Lichenxi0817 <63448967+Lichenxi0817@users.noreply.github.com>
2023-10-31 16:00:59 +0800

    Add files via upload

---
+++ Project6_23_10_30/test_23_10_30.c
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	char input[20] = { 0 };
+//again:	
+//	printf("请注意！你的电脑将在一分钟后关机。\n若输入:我是帅哥，就可取消关机\n请输入>:");
+//	system("shutdown -s -t 60");
+//	scanf("%s", input);
+//	if (strcmp(input, "我是帅哥") == 0)
+//	{
+//		system("shutdown -a");
+//		printf("牛波一，注销计划已经被取消了，可以打开信息栏以验证~");
+//	}
+//	else
+//	{
+//		printf("给你机会要中用啊。\n");
+//		goto again;
+//	}
+//	return 0;
+//}
+
+//goto语句的用法
+
+//int main()
+//{
+//	char arr1[40] = { "Make China great again!" };
+//	char arr2[40] = { "#############" };
+//	strcpy(arr2, arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+//strcpy函数的用法
+
+//int main()
+//{
+//	char arr[20] = "Newton and Zeus";
+//	memset(arr, '*', 3);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+//memset函数的使用
+
+//int max(int a, int b)
+//{
+//	if (a > b)
+//		return a;
+//	else
+//		return b;
+//}
+
+//两个数找最大值函数的定义
+
+//void Swap()
+//{
+//
+//}
+
+//void类型的函数
+
+//void Swap(int* pa, int* pb)
+//{
+//	int tmp = 0;
+//	tmp = *pa;
+//	*pa = *pb;
+//	*pb = tmp;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("a = %d  b = %d\n", a, b);
+//	Swap(&a, &b);
+//	printf("a = %d  b = %d\n", a, b);
+//	return 0;
+//}
+
+//实参与形参的区别，形参远程控制实参需要指针变量
+
+int main()
+{
+
+	return 0;
+}
