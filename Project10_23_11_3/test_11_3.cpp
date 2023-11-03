b3de42bbd37e8d8a752338ae08c917bb2c00bfef
Lichenxi0817 <63448967+Lichenxi0817@users.noreply.github.com>
2023-11-03 21:54:13 +0800

    Add files via upload

---
+++ Project10_23_11_3/test_11_3.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<string.h>
+
+//int main() {
+//	char arr[] = "abcdef";
+//	int num[] = { 1,2,3,4,5,6 };
+//	printf("%d\n", sizeof(float));
+//	printf("%d\n", sizeof(num));
+// 	printf("%d\n", sizeof(arr));//此处误认为是28
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+
+//char和int形所占空间不同，并非4 4，而是1 4；我悟了
+
+//int main() {
+//	int arr[3][4] = { {1,2,3},4,5 };
+//	return 0;
+//}
+
+//二维数组
+
+//int main() {
+//	int arr[3][4] = { {1,2,3},{4,5} };//数组不论几维都需要常(变)量来定义
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 4; j++) {
+//			printf("%d ", arr[i][j]);//此处是一个引用,并非定义,望知晓
+//		}
+//		printf("\n");
+//	}
+//	printf("%d", arr);//arr是一个数组，本质上还是一个指针，所以这里输出的是arr的地址的10进制表示
+//	return 0;
+//}
+
+//定义中的一些问题,以及多维数组的打印
+
+int main() {
+
+	return 0;
+}
