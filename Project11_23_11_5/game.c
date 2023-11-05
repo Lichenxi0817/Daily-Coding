f7ef0039c4e9a08bbb690a90417e7fa9974aa9b0
Lichenxi0817 <63448967+Lichenxi0817@users.noreply.github.com>
2023-11-05 16:28:52 +0800

    Add files via upload
    未完成项目，有很多问题等待完成，明天再来进行更改，今天太困😴了

---
+++ Project11_23_11_5/game.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include"game.h"
+#define ROW 3;
+#define COL 3;
+
+void InitBoard(char board[ROW][COL], int row, int col)
---
+++ Project11_23_11_5/game.h
@@ -0,0 +1,11 @@
+#pragma once
+#include"game.h"
+#include "game.c"
+
+void InitBoard(char board[ROW][COL], int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			board[i][j] = ' ';
+		}
+	}
+}
