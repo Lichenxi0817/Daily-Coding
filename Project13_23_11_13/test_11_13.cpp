3c9b419e4f34e0f602fa89d1f7f9918a801f927f
Lichenxi0817 <63448967+Lichenxi0817@users.noreply.github.com>
2023-11-13 15:15:41 +0800

    Add files via upload
    数组删除算法的实现（由于在恶补数据结构课，没有太注意学C，┭┮﹏┭┮

---
+++ Project13_23_11_13/test_11_13.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+//int ListDelete(int arr[], int sz, int i, int e) {
+//	if (arr[i] != e)
+//		return 0;
+//	else
+//	{
+//		for (i; i <= sz - 2; i++)
+//			arr[i] = arr[i + 1];
+//		return 1;
+//	}
+//}
+//
+//int main() {
+//	int arr[] = { 1,2,3,6,8,9,5,11,225,662,55,44,15,887,78,9,10,21 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 11;
+//	int e = 44;
+//	if (ListDelete(arr, sz, i, e) == 0)
+//		printf("没找到这个对应的元素\n");
+//	else
+//	{
+//		printf("找到了，已将这个元素删除\n现在这个数组是 arr = ");
+//		for (int n = 0; n < sz; n++) {
+//			printf("%d ", arr[n]);
+//		}
+//	}
+//	return 0;
+//}
+
+//数组删除算法的实现
+
+int main() {
+	
+	return 0;
+}
